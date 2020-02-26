4e613b9abe468c66701cab112332d5bfd896c8e4
zmathew <zmathew@ncsu.edu>
2020-02-26 17:12:34 -0500

    Trigger a build using the pipeline build command

---
+++ commands/build.js
@@ -0,0 +1,62 @@
+const jenkins = require("jenkins")({
+  baseUrl: "http://admin:admin@192.168.33.20:9000",
+  crumbIssuer: true,
+  promisify: true
+});
+
+exports.command = "build <job>";
+exports.desc = "Trigger the build job";
+
+async function getBuildStatus(job, id) {
+  return new Promise(async function(resolve, reject) {
+    console.log(`Fetching ${job}: ${id}`);
+    let result = await jenkins.build.get(job, id);
+    resolve(result);
+  });
+}
+
+async function waitOnQueue(id) {
+  return new Promise(function(resolve, reject) {
+    jenkins.queue.item(id, function(err, item) {
+      if (err) throw err;
+      if (item.executable) {
+        console.log("number:", item.executable.number);
+        resolve(item.executable.number);
+      } else if (item.cancelled) {
+        console.log("cancelled");
+        reject("canceled");
+      } else {
+        setTimeout(async function() {
+          resolve(await waitOnQueue(id));
+        }, 5000);
+      }
+    });
+  });
+}
+
+async function triggerBuild(job) {
+  let queueId = await jenkins.job.build(job);
+  let buildId = await waitOnQueue(queueId);
+  return buildId;
+}
+
+async function main() {
+  var job = process.argv[3];
+  console.log(`Triggering build: ${job}`);
+  let buildId = await triggerBuild(job).catch(e => console.log(e));
+
+  console.log(`Received ${buildId}`);
+  let build = await getBuildStatus(job, buildId);
+  console.log(`Build result: ${build.result}`);
+
+  console.log(`Build output`);
+  let output = await jenkins.build.log({
+    name: job,
+    number: buildId
+  });
+  console.log(output);
+}
+
+(async () => {
+  await main();
+})();
