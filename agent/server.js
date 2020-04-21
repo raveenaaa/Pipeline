bbf5a2ba43bc1c4810a1621e7fc61d60d266f71a
Raveena D'costa <raveenadcosta19@gmail.com>
2020-04-21 03:33:26 -0400

    Deleted server.js

--- agent/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3001
-const host = 'localhost';
-
-app.get('/', (req, res) => res.send('Server is running...'));
-
-app.listen(port, () => console.log(`Example app listening at http://${host}:${port}`))
